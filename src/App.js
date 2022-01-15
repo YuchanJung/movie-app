4c2abb5e3f2d86c2da1be2436b021c716aff2498
yuchannn <yuchanbpp@gmail.com>
2022-01-15 22:47:15 +0900

    react Cleanup prac

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, [])
+  return (
+    <h1>Hello</h1>
+  );
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
   // console.log("i run all the time."); // state가 변할 때마다 매번 실행됨
+  const onClickShowing = () => setShowing((prev) => !prev);
   useEffect(() => {
     console.log("I run only once.");
   }, []);
@@ -20,7 +32,7 @@ function App() {
     }
   }, [keyword]); // keyword가 변화할때만 코드를 실행함. counter가 변화할 때는 실행 x
   useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.");
+    console.log("I run when 'keyword | counter' changes.");
   }, [keyword, counter]);
   return (
     <div>
@@ -28,6 +40,8 @@ function App() {
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
       <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClickShowing}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
