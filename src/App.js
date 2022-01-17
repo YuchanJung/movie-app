7e39e799e5e698bec735664297474b83a873a8dd
yuchannn <yuchanbpp@gmail.com>
2022-01-17 16:06:06 +0900

    making ToDoList

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]); // state를 직접 수정하지 않음. 항상 func 사용
+  const [toDos, setToDos] = useState([]); // array도 state를 직접 수정하지 않음. 항상 func 사용
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
@@ -13,11 +13,19 @@ function App() {
     }
     else {
       setToDo("");
-      setToDos((prev) => [toDo, ...toDos]);
+      setToDos((currentArray) => {
+        const newToDo = "my new todo is " + toDo;
+        return [newToDo, ...currentArray];
+      }
+      );
     }
   };
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos])
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -27,6 +35,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li> // key props 넣어주어야 console 상 오류 나지 않음
+        ))}
+      </ul>
     </div>
   );
 }
