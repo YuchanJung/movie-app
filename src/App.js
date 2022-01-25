a875d1b04ceb9e910fec93ad61e1ce476a70c989
yuchannn <yuchanbpp@gmail.com>
2022-01-25 21:21:17 +0900

    make Movie component with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie.js";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const json = await(
+  const getMovies = async () => {
+    const json = await (
       await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
     ).json();
     setMovies(json.data.movies);
@@ -15,23 +16,18 @@ function App() {
   }, [])
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : 
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            {movie.hasOwnProperty("genres") ? 
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            : null
-            } 
-          </div>
-        ))}
-      </div>}
+      {loading ? <h1>Loading...</h1> :
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>}
     </div>
   );
 }
